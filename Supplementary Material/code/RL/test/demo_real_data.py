5b6a982db069c7e58ed70d0bf4f9a3af000bb241
JGcuzme <103177367+JGcuzme@users.noreply.github.com>
2023-05-08 10:02:04 +0800

    Update demo_real_data.py
    update

--- Supplementary Material/code/RL/test/demo_real_data.py
+++ Supplementary Material/code/RL/test/demo_real_data.py
@@ -4,14 +4,15 @@ import pandas as pd
 import numpy as np
 from recode.causal_strength import adj_cs
 
-# 导入数据
-X = pd.read_csv("C:/Users/14903/Desktop/data/768/原始数据集/768.csv")
-X = np.array(X.drop(columns="Outcome"))
 
-# 训练学习
+X = pd.read_csv("data/768.csv")
+# X = pd.read_csv("data/768_1.csv")
+X = np.array(X.drop(columns="Outcome"))
+# X = np.array(X.drop(columns="Outcome"))
+
 rl = RL(nb_epoch=20000, device_type="gpu", score_type="BIC")
 rl.learn(X)
 print(rl.causal_matrix)
 print(adj_cs(rl.causal_matrix, X))
-# 显示图和计算指标
+
 output_adj(rl.causal_matrix)
